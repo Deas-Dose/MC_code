ab46bccee1f9d51e7e3d3de33e2a063607b6262a
deas dose <deasdose@proton.me>
2023-04-16 02:11:36 +0100

    finished 2nd_day

---
+++ 2nd_day/num_converter.py
@@ -0,0 +1,24 @@
+# a: to_roman function, b: arg, c: output of function, d: partial date, e: full date
+# f: integer, g: result
+
+
+f,s="1100 V/III/MCMLXXXV".split()
+r={1000:'M',500:'D',100:'C',50:'L',10:'X',5:'V',1:'I'}
+z={v:k for k,v in r.items()}
+e=s.split('/')
+f=int(f)
+g=["",0]
+for x in r:
+    while f>=x:
+        g[0]+=r[x]
+        f-=x
+def a(b):
+    c=0
+    for i in range(len(b)):
+        if i+1!=len(b) and z[b[i]]<z[b[i+1]]:
+            c-=z[b[i]]
+        else:
+            c+=z[b[i]]
+    return str(c)
+g[1]="/".join(a(d) for d in e)
+print(" ".join(g))
